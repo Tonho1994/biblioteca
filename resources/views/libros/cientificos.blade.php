025e5fe878b85eed10f3840e08390076cbfcfdb0
Tonho1994 <tono.morazu.94@gmail.com>
2019-11-23 23:30:48 -0600

    modificado:     resources/views/libros/cientificos.blade.php

--- resources/views/libros/cientificos.blade.php
+++ resources/views/libros/cientificos.blade.php
@@ -3,16 +3,12 @@
 @section('content')
 <div class="container-fluid">
 
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1 class="jumbotron-heading">Album example</h1>
-      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-      <p>
-        <a href="#" class="btn btn-primary my-2">Main call to action</a>
-        <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-      </p>
+    <div class="row col-lg-12 jumbotron">
+        <h1 class="col-lg-12 text-center">Científicos</h1>
+        <p class="col-lg-12 text-muted text-center">
+            Matematicas, Física, Historia, Psicología, Química, Geografía, Biología, Medicina, Astronomía, Antropología, Politíca, Economía,  Computación, Lógica
+        </p>
     </div>
-  </section>
 
   <div class="album py-5 bg-light">
     <div class="container">
@@ -20,15 +16,13 @@
       <div class="row">
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Vista previa</text></svg>
             <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text">Aqui es donde ira la informacion del libro en cuestion,    titulo, autor, usuario que lo subio</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-                <small class="text-muted">9 mins</small>
+
               </div>
             </div>
           </div>
